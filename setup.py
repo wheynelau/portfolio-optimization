3e7ee524c5e2b979878e6665c7a71fa67aea9283
Wayne Lau <90563815+wheynelau@users.noreply.github.com>
2022-05-23 01:33:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,123 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open(file="README.md", mode="r") as fh:
-    long_description = fh.read()
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
 
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'PyOpt'
+DESCRIPTION = 'Portfolio optimization with stocks and cryptocurrencies'
+URL = 'https://github.com/wheynelau/portfolio-optimization/'
+EMAIL = 'me@example.com'
+AUTHOR = 'Wayne'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '0.1.0'
+
+REQUIRED = [
+    'yfinance','numpy','pandas','matplotlib','scipy'
+]
+
+}
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='py-portfolio',
-    author='Alex Reed',
-    author_email='coding.sigma@gmail.com',
-    version='0.0.1',
-    description='A python application, that demonstrates optimizing a portfolio using machine learning.',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/areed1192/portfolio-optimization',
-    install_requires=[
-        'requests==2.24.0',
-        'matplotlib==3.3.0',
-        'numpy==1.19.0',
-        'pandas==1.0.5',
-        'scipy==1.5.2',
-        'fake_useragent==0.1.11',
-        'python_dateutil==2.8.1',
-        'scikit_learn==0.23.2'
-    ],
-    packages=find_packages(
-        include=['pyopt']
-    ),
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Financial and Insurance Industry',
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    python_requires='>3.7'
-)
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
